9dad16bdd8ccf32035ad5942e567e8c50c645762
P1kkuMikko <mikko.nyk@gmail.com>
2024-12-08 23:27:15 +0200

    Esedun matksun pohjalta tehtyä

--- osa1/tehtävä 1.1-1.2/src/App.jsx
+++ osa1/tehtävä 1.1-1.2/src/App.jsx
@@ -1,13 +1,20 @@
 const Header = ({ course }) => {
+  console.log(props);
   return <h1>{course}</h1>;
 };
 
 const Content = (props) => {
   return (
     <div>
-      <p>{props.part1} {props.exercises1}</p>
-      <p>{props.part2} {props.exercises2}</p>
-      <p>{props.part3} {props.exercises3}</p>
+      <p>
+        {props.part1} {props.exercises1}
+      </p>
+      <p>
+        {props.part2} {props.exercises2}
+      </p>
+      <p>
+        {props.part3} {props.exercises3}
+      </p>
     </div>
   );
 };
@@ -28,14 +35,21 @@ const App = () => {
   return (
     <div>
       <Header course={course} />
-      <Content 
-        part1={part1} exercises1={exercises1} 
-        part2={part2} exercises2={exercises2} 
-        part3={part3} exercises3={exercises3} 
+      <Content
+        part1={part1}
+        exercises1={exercises1}
+        part2={part2}
+        exercises2={exercises2}
+        part3={part3}
+        exercises3={exercises3}
       />
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3} />
+      <Total
+        exercises1={exercises1}
+        exercises2={exercises2}
+        exercises3={exercises3}
+      />
     </div>
   );
-}
+};
 
 export default App;
