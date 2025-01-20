362e9c274ce9f0ba289c11c00b98f4e161e63dd2
P1kkuMikko <mikko.nyk@gmail.com>
2025-01-20 14:29:29 +0200

    osa 2a valmis, tehtävät 2.1.-2.5. valmiit

---
+++ osa1/part1 (reactin alkeet)/src/components/Course.jsx
@@ -0,0 +1,31 @@
+const Header = ({ course }) => <h1>{course}</h1>;
+
+const Part = ({ part }) => (
+  <p>
+    {part.name} {part.exercises}
+  </p>
+);
+
+const Content = ({ parts }) => (
+  <div>
+    {parts.map((part) => (
+      <Part key={part.id} part={part} />
+    ))}
+  </div>
+);
+
+const Total = ({ parts }) => (
+  <p>
+    Number of exercises {parts.reduce((sum, part) => sum + part.exercises, 0)}
+  </p>
+);
+
+const Course = ({ course }) => (
+  <div>
+    <Header course={course.name} />
+    <Content parts={course.parts} />
+    <Total parts={course.parts} />
+  </div>
+);
+
+export default Course;
