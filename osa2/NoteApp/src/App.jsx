4a2669f43f22747d385ddcf5bb7f7d35357c955a
P1kkuMikko <mikko.nyk@gmail.com>
2025-01-21 20:50:02 +0200

    osa2b ja tehtävät 2.6.-2.10.

--- osa2/NoteApp/src/App.jsx
+++ osa2/NoteApp/src/App.jsx
@@ -4,6 +4,7 @@ import { useState } from 'react';
 const App = (props) => {
   const [notes, setNotes] = useState(props.notes);
   const [newNote, setNewNote] = useState('');
+  const [showAll, setShowAll] = useState(true);
 
   const addNote = (event) => {
     event.preventDefault();
@@ -21,11 +22,20 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
+  const notesToShow = showAll
+    ? notes
+    : notes.filter((note) => note.important === true);
+
   return (
     <div>
       <h1>Notes</h1>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          Show {showAll ? 'important' : 'all'}
+        </button>
+      </div>
       <ul>
-        {notes.map((note) => (
+        {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
         ))}
       </ul>
---
+++ osa2/Puhelinluettelo/src/components/Persons.jsx
@@ -0,0 +1,13 @@
+const Persons = ({ personsToShow }) => {
+  return (
+    <div>
+      {personsToShow.map((person, index) => (
+        <div key={index}>
+          {person.name} {person.number}
+        </div>
+      ))}
+    </div>
+  );
+};
+
+export default Persons;
